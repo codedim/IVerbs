896927ad5fb42424daa98a7497580628d94a4b31
codedim <codedim@gmail.com>
2016-10-24 07:23:27 +0300

    added: a few svg graphics

--- mobile.js
+++ mobile.js
@@ -3,30 +3,64 @@ function isSet(testVar) {
 	return !(typeof(testVar) == 'undefined' || testVar === null);
 }
 
-function fullScreen() {
-	const barHeight = 50; // height of mobile browser menu bar 
+window.onload = function() {
+	var fullScreenBnt = document.getElementById('fullScreenBnt');
+	if (isSet(fullScreenBnt)) {
+		fullScreenBnt.addEventListener('mousedown', addResizer, false);
+		fullScreenBnt.addEventListener('touchstart', addResizer, false);
 
-	var body = document.getElementsByTagName('body')[0];
+		fullScreenBnt.addEventListener('mouseup', freeResizer, false);
+		fullScreenBnt.addEventListener('mouseout', freeResizer, false);
+		fullScreenBnt.addEventListener('touchend', freeResizer, false);
+	}
+}
+
+window.onscroll = function() {
+	holdBodyPosition();
+}
+
+function addResizer() {
+	if (!isSet(fullScreenBnt)) return;
+
+	var body = document.body
 	if (!isSet(body)) return;
 
 	var resizer = document.createElement('div');
 	body.appendChild(resizer);
+	resizer.id = 'scrResizer';
 
 	resizer.style.position = 'absolute';
 	resizer.style.top = '0px';
 	resizer.style.height = '100vh';
-
-resizer.style.width = barHeight + 'px';
-resizer.style.backgroundColor = '#ccc';
+	resizer.style.width = '1px';
 
 	var scrHeight = resizer.clientHeight;
-    resizer.style.minHeight = scrHeight + barHeight + 'px';
+    resizer.style.minHeight = (scrHeight  * 20) + 'px';
+};
 
-	var timer, scroller = 0;
-	function scrollBy() {
-		window.scrollTo(0, scroller);		
-		if (++scroller >= barHeight) clearInterval(timer);
-	}
+function freeResizer() {
+	if (!isSet(fullScreenBnt)) return;
 
-	timer = setInterval(scrollBy, 40);
+	var body = document.body;
+	var resizer = document.getElementById('scrResizer');
+	if (!isSet(body) || !isSet(resizer)) return;
+
+	setTimeout(function() {
+		while (resizer) {
+			body.removeChild(resizer);
+			resizer = document.getElementById('scrResizer');
+		}
+		freeBodyPosition();
+	}, 600);
+};
+
+function holdBodyPosition() {
+	if (!document.getElementById('scrResizer')) return;
+
+	var body = document.body;
+	body.style.marginTop = body.scrollTop;
+}
+
+function freeBodyPosition() {
+	document.body.style.marginTop = '';
 }
